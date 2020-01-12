fd97ab2764c8a04abd76e9e9c146bb15c7836447
Edwin Keijl <e.keijl89@gmail.com>
2020-01-13 00:13:58 +0100

    Fix manifest, serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+registerServiceWorker();
